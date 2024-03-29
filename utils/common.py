52d0701e56aa94f60763bdb20a13136bf3642f5d
Celocious Altrin <celociousalltrinmf@gmail.com>
2024-03-29 09:15:04 +0530

    error custom instance implemented

--- utils/common.py
+++ utils/common.py
@@ -1,6 +1,7 @@
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError,NotFound
 from rest_framework import status
 from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 
 def get_child_data(data,keys):
     if data:
@@ -15,6 +16,9 @@ def handleError(e):
     if isinstance(e, ValidationError):
         print("Validation error:", e)
         return Response({"error": "Validation error", "details": str(e)}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    elif isinstance(e,ObjectDoesNotExist):
+        print("Not Found",e)
+        return Response({"error": "Not Found Error", "details": str(e)}, status=status.HTTP_417_EXPECTATION_FAILED)
     else:
         print("Error:", e)
         return Response({"error": "An unexpected error occurred"}, status=status.HTTP_406_NOT_ACCEPTABLE)
